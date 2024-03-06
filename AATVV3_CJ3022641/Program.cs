c0753ea3ec9cd5388bf3857ce07d47b8fcbe012d
CJ3022641 <CJ3022641@LAB01-PC35>
2024-03-06 09:00:56 -0300

    Adicionar arquivos de projeto.

---
+++ AATVV3_CJ3022641/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AATVV3_CJ3022641
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite sua nota");
+            double nota =
+                 Convert.ToDouble(Console.ReadLine());
+
+            //verificação se a nota é maior de 6
+            if (nota > +6)
+            {
+                Console.WriteLine("O aluno passou! ");
+            }
+            else
+            {
+                Console.WriteLine("O aluno não passou ");
+            }
+        }
+    }
+}
